e80329a805af5c633d8587863886f86c01c1dde4
matias <matias_arenhard@hotmail.com>
2018-04-16 14:20:26 -0300

    development off api

---
+++ app/models/contact.rb
@@ -0,0 +1,4 @@
+class Contact < ApplicationRecord
+  validates :name, :user, presence: true
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180414211329) do
+ActiveRecord::Schema.define(version: 20180416160654) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
